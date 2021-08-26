a37a26f22197834720ecc85155086c0b4f7561ee
JairNeto1 <jairmonteironeto@gmail.com>
2021-08-26 10:53:52 -0300

    first commit

---
+++ src/modulo1.js
@@ -0,0 +1,15 @@
+var hora = 12
+
+if (hora >= 12 && hora < 18) {
+  console.log('boa tarde!')
+}
+if (hora >= 0 && hora < 12) {
+  console.log('Bom dia!')
+}
+if (hora >= 18 && hora <= 24) {
+  console.log('Boa noite!')
+}
+
+// var   -> escopo geral
+// let   -> escopo específoco
+// const -> variável de valor constante
