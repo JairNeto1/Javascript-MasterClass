67060aefd60b8fb53db44eac3b297636606fd639
JairNeto1 <jairmonteironeto@gmail.com>
2021-08-28 17:24:18 -0300

    color-change-app

--- src/modulo2/dom.js
+++ src/modulo2/dom.js
@@ -1,1 +1,17 @@
-document.getElementById('teste').innerHTML = 'Marcos'
+function blue() {
+  document.querySelector('.box').classList.remove('red')
+  document.querySelector('.box').classList.remove('orange')
+  document.querySelector('.box').classList.add('blue')
+}
+
+function red() {
+  document.querySelector('.box').classList.remove('blue')
+  document.querySelector('.box').classList.remove('orange')
+  document.querySelector('.box').classList.add('red')
+}
+
+function orange() {
+  document.querySelector('.box').classList.remove('red')
+  document.querySelector('.box').classList.remove('blue')
+  document.querySelector('.box').classList.add('orange')
+}
