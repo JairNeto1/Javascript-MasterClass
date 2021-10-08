ca4fd3150db79e3f1e77560f84d44d976f858dde
JairNeto1 <jairmonteironeto@gmail.com>
2021-10-08 10:53:07 -0300

    deleting js unuesd files

--- logica/case3.js
+++
@@ -1,9 +0,0 @@
-var userinset = 5;
-
-function factorial(n) {
-  var resultado = n;
-  for (var i = 1; i < n; i++)
-  
-  return resultado;
-}
-console.log(factorial(userinset))
--- logica/case5.js
+++
@@ -1,9 +0,0 @@
-var numeros = [
-  1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20
-]
-
-if (numeros % 2 == 0) {
-  console.log(par)
-} else if{
-  console.log(impar)
-}
